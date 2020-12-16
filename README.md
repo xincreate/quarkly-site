c7b88a1ffcc7d11398f9f1c571306c9893a31a10
Xinxin Liu <liuxinxin.design@gmail.com>
2020-12-16 20:02:04 +0800

    Changes

--- README.md
+++ README.md
@@ -15,6 +15,11 @@
     <img src="https://badgen.net/badge/Gatsby/2.23.12/purple" alt="Gatsby version" />
   </a>
   
+   <a href="https://github.com/xincreate/quarkly-site/pulls">
+     <img src="https://badgen.net/github/prs/xincreate/quarkly-site" alt="Pull Request Amount">
+   </a>
+   <img src="https://badgen.net/github/last-commit/xincreate/quarkly-site" alt="Last commit date" />
+    
   <a href="./LICENSE">
     <img src="https://badgen.net/badge/license/MIT/blue" alt="License">
   </a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Theme, Link, Text } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
-import { Override, Menu, Section, StackItem, Stack } from "@quarkly/components";
+import { Section } from "@quarkly/components";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -12,74 +12,37 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
-		<Section>
-			<Box
-				display="flex"
-				padding="12px 0"
-				justify-content="space-between"
-				align-items="center"
-				flex-direction="row"
-				md-flex-direction="column"
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			min-height="600px"
+			sm-min-height="auto"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Text
+				font="--base"
+				text-transform="uppercase"
+				letter-spacing="1px"
+				max-width="850px"
+				color="--grey"
+				margin="0px"
 			>
-				<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--lead">
-					Awesome Company
-				</Text>
-				<Menu
-					display="flex"
-					justify-content="center"
-					font="--base"
-					font-weight="700"
-					md-flex-direction="column"
-					md-align-items="center"
-				>
-					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
-					<Override slot="link-active" color="--primary" />
-					<Override slot="item" padding="6px" />
-				</Menu>
-			</Box>
-		</Section>
-		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
-			<Stack>
-				<StackItem width="50%" lg-width="100%">
-					<Override
-						slot="StackItemContent"
-						margin-right="-20%"
-						padding-bottom="120%"
-						background="url(https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) 50% 15%/cover"
-						lg-margin-right="0"
-						lg-padding-bottom="80%"
-					/>
-				</StackItem>
-				<StackItem width="50%" lg-width="100%">
-					<Override
-						slot="StackItemContent"
-						padding="98px 64px"
-						margin-left="-20%"
-						margin-top="36px"
-						background="--color-red"
-						color="--light"
-						mix-blend-mode="lighten"
-						lg-margin-left="0"
-						lg-margin-top="0"
-						flex-direction="column"
-					/>
-					<Text
-						as="h4"
-						margin="0"
-						font="--base"
-						letter-spacing="1px"
-						text-transform="uppercase"
-					>
-						About me
-					</Text>
-					<Text as="h1" margin="0 0 16px 0" font="--headline1" lg-font="--headline2">
-						Hey I’m David
-					</Text>
-					<Text as="p" margin="16px 0" font="--base" max-width="400px">
-						My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
-					</Text>
-				</StackItem>
-			</Stack>
+				ABOUT
+			</Text>
+			<Text
+				as="h1"
+				margin="8px 0px 32px"
+				font="--headline1"
+				md-font="--headline2"
+				color="--dark"
+				max-width="850px"
+			>
+				您好，我是一个专注于提升用户体验的UI设计师。
+				<br />
+				欢迎合作！
+			</Text>
 		</Section>
 		<Link
 			font={"--capture"}
